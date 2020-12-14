152421756920ba0b4751f6d93396cc6feed64361
softdevws <softdevws@yahoo.com>
2020-12-14 13:38:38 +0530

    mobile header and footer

--- src/layouts/LayoutFull.jsx
+++ src/layouts/LayoutFull.jsx
@@ -3,29 +3,27 @@ import React from "react"
 import { Footer } from "../components"
 
 export const LayoutFull = ({ children }) => {
-
   return (
     <div className="bg-gray-100 dark:bg-gray-900">
       <header className="bg-white dark:bg-transparent py-6">
         <div className="container">
           <div className="flex flex-wrap -mx-3 xl:-mx-6">
-            <div className="flex sm:w-1/3 xl:w-1/3 px-3 xl:px-6">
+            <div className="flex w-1/2 sm:w-1/2 xl:w-1/2 px-3 xl:px-6">
               <Link to="/" className="inline-block">
                 <img alt="Logo" src="/logo.svg" width="150px" />
               </Link>
             </div>
-            <div className="flex sm:w-1/3 xl:w-1/3 px-3 xl:px-6">
-              &nbsp;
-            </div>
-            <div className="flex sm:w-1/3 xl:w-1/3 px-3 xl:px-6 justify-center items-end">
-              <Link to="/search" className="soklink">Search</Link>
+            <div className="flex w-1/2 sm:w-1/2 xl:w-1/2 px-3 xl:px-6 justify-center items-end">
+              <Link to="/search" className="soklink">
+                Search
+              </Link>
             </div>
           </div>
         </div>
       </header>
 
       {children}
-      
+
       <Footer />
     </div>
   )
--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -2,21 +2,19 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Cards, SiteMetadata } from "../components"
 import { Layout } from "../layouts/Layout"
-import { Link } from 'gatsby'
-import 'lazysizes';
+import { Link } from "gatsby"
+import "lazysizes"
 
 export default (props) => {
+  const { data, pageContext } = props
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
 
-    const { data, pageContext } = props
-    const { currentPage, numPages } = pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
-    const nextPage = (currentPage + 1).toString()
-    
   return (
     <Layout>
-
       <SiteMetadata
         title="Wiki Hospitals"
         description="A site that displays the latest in international health technology along with blog posts about patients stories."
@@ -25,34 +23,34 @@ export default (props) => {
 
       <div className="container pt-6 overflow-hidden">
         <h1 style={{ marginTop: `3em`, textAlign: `left` }}>
-          A site that displays the latest in international health technology along with blog posts about patients stories.
+          A site that displays the latest in international health technology
+          along with blog posts about patients stories.
         </h1>
       </div>
 
       <Cards nodes={data.items.nodes} />
 
-          <div className="container pt-6 overflow-hidden">
-              <div className="flex flex-wrap -mx-3 xl:-mx-6">
-                <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6 text-right">
-                    {!isFirst && (
-                      <Link to={`${(prevPage==="/" ? prevPage : "/page/" + prevPage)}`} rel="prev">
-                          ← Previous Page
-                      </Link>
-                  )}
-                </div>
-                <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6">
-                    &nbsp;
-                </div>
-                <div className="w-full sm:w-1/2 xl:w-1/3 px-3 xl:px-6 py-6 sm:text-left">
-                {!isLast && (
-                      <Link to={`/page/${nextPage}`} rel="next">
-                          Next Page →
-                      </Link>
-                  )}
-                </div>
-              </div>
+      <div className="container pt-6 overflow-hidden">
+        <div className="flex flex-wrap -mx-3 xl:-mx-6">
+          <div className="w-1/2 sm:w-1/2 xl:w-1/2 px-3 xl:px-6 py-6 text-left">
+            {!isFirst && (
+              <Link
+                to={`${prevPage === "/" ? prevPage : "/page/" + prevPage}`}
+                rel="prev"
+              >
+                ← Previous Page
+              </Link>
+            )}
           </div>
-
+          <div className="w-1/2 sm:w-1/2 xl:w-1/2 px-3 xl:px-6 py-6 text-right">
+            {!isLast && (
+              <Link to={`/page/${nextPage}`} rel="next">
+                Next Page →
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
@@ -63,16 +61,16 @@ export const query = graphql`
       ...HeroImageFragment
     }
     items: allAirtable(
-      limit: $limit,
-      skip: $skip,
+      limit: $limit
+      skip: $skip
       filter: { table: { eq: "Health companies" } }
-      ) {
+    ) {
       nodes {
         data {
-          country : Country_of_origin
-          name : Company_title
+          country: Country_of_origin
+          name: Company_title
           slug
-          description : Description
+          description: Description
           image {
             url
           }
